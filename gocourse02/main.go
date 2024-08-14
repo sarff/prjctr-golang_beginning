67c7911837651c375483b46f063020a70df63395
Dmytro F <exehoua@gmail.com>
2024-08-14 23:06:00 +0300

    corrections in accordance with the latest revisions

--- gocourse02/main.go
+++ gocourse02/main.go
@@ -20,7 +20,7 @@ type ZooKeeper struct {
 	AnimalsCaught int
 }
 
-type Catcher interface {
+type _ interface {
 	FindEscapedAnimal() bool                // Спроба знайти звіра
 	AttemptCatchAnimal(*Cage, *Animal) bool // Сброба зловити звіра в клітку
 }
@@ -37,7 +37,7 @@ type Cage struct {
 	MaxWeight     int
 	CurrentSeats  int
 	CurrentWeight int
-	Animal        []Animal
+	Animals       []Animal
 }
 
 func NewAnimal(id, weight int) *Animal {
@@ -46,20 +46,21 @@ func NewAnimal(id, weight int) *Animal {
 }
 
 func NewZooKeeper(name string) *ZooKeeper {
-	return &ZooKeeper{name, 0, 0}
+	return &ZooKeeper{Name: name, AnimalsFound: 0, AnimalsCaught: 0}
 }
 
-func NewCage(animNumber int) *Cage {
-	numberSeats := RandInt(1, animNumber+1)    // Дамо звірям шанс
+func NewCage(capacity int) *Cage {
+	numberSeats := RandInt(1, capacity+1)      // Дамо звірям шанс
 	maxWeight := numberSeats * RandInt(10, 50) // Максимальная вага одного звіра в зоопарку 50
-	animals := make([]Animal, 0)
-	return &Cage{numberSeats, maxWeight, 0, 0, animals}
+	// numberSeats+1 - Максимальная кількість звірів яка може поміститися в клітку +1=child
+	animals := make([]Animal, 0, numberSeats+1)
+	return &Cage{MaxSeats: numberSeats, MaxWeight: maxWeight, CurrentSeats: 0, CurrentWeight: 0, Animals: animals}
 }
 
-func (k *ZooKeeper) SearchingForAnimalsReturnToCage(catch Catcher, animal *Animal, cage *Cage) {
-	if catch.FindEscapedAnimal() {
+func (k *ZooKeeper) SearchForAnimalAndReturnToCage(animal *Animal, cage *Cage) {
+	if k.FindEscapedAnimal() {
 		k.AnimalsFound++
-		if catch.AttemptCatchAnimal(cage, animal) {
+		if k.AttemptCatchAnimal(cage, animal) {
 			k.AnimalsCaught++
 			cage.CurrentSeats++
 		}
@@ -75,21 +76,28 @@ func (k *ZooKeeper) FindEscapedAnimal() bool {
 func (k *ZooKeeper) AttemptCatchAnimal(cage *Cage, animal *Animal) bool {
 	if cage.MaxWeight >= animal.Weight+cage.CurrentWeight && cage.MaxSeats >= cage.CurrentSeats+1 {
 		cage.CurrentWeight += animal.Weight
-		cage.Animal = append(cage.Animal, *animal)
+		cage.Animals = append(cage.Animals, *animal)
 		return true
 	}
 	return false
 }
 
-// Функція розможноження
-func (male *Animal) Reproduction(female *Animal) {
+// Функція розмноження
+func (male *Animal) Reproduction(female *Animal) *Animal {
 	avgWeight := (male.Weight + female.Weight) / 2
-	child := NewAnimal(escapedAnimalsCount, avgWeight)
-	fmt.Printf("Replay between %s and %s is complete!! Congratulations to the new animal %v \n", male.Name, female.Name, *child)
+	if rand.N(2) == 1 {
+		//child.id завжди буде +1 від константи escapedAnimalsCount, це прописано в функції NewAnimal
+		child := NewAnimal(escapedAnimalsCount, avgWeight)
+		return child
+	} else {
+		return nil
+	}
+
 }
 
-func RandInt(minValue, maxValue int) int {
-	return rand.IntN(maxValue-minValue) + minValue
+// RandInt generates integer from <low, high> interval.
+func RandInt(low, high int) int {
+	return rand.IntN(high-low) + low
 }
 
 func main() {
@@ -98,14 +106,15 @@ func main() {
 
 	for i := 0; i < escapedAnimalsCount; i++ {
 		animal := NewAnimal(i, RandInt(10, 50))
-		keeper.SearchingForAnimalsReturnToCage(keeper, animal, cage)
+		keeper.SearchForAnimalAndReturnToCage(animal, cage)
 	}
 
-	fmt.Printf("Zookeeper found %d and caught %d animals Out of %d\n", keeper.AnimalsFound, keeper.AnimalsCaught, escapedAnimalsCount)
+	fmt.Printf("Zookeeper found %d and caught %d animals "+
+		"Out of %d\n", keeper.AnimalsFound, keeper.AnimalsCaught, escapedAnimalsCount)
 
 	// for reproduction:
 	var male, female = -1, -1
-	for i, v := range cage.Animal {
+	for i, v := range cage.Animals {
 		switch v.Gender {
 		case "male":
 			if male == -1 {
@@ -120,7 +129,14 @@ func main() {
 	}
 
 	if male > -1 && female > -1 {
-		cage.Animal[male].Reproduction(&cage.Animal[female])
+		child := cage.Animals[male].Reproduction(&cage.Animals[female])
+		if child != nil {
+			fmt.Printf("Replay between %s and %s is complete!! "+
+				"Congratulations to the new animal %v \n", cage.Animals[male].Name, cage.Animals[female].Name, *child)
+		} else {
+			fmt.Printf("%s and %s failed to give birth "+
+				"to a cub\n", cage.Animals[male].Name, cage.Animals[female].Name)
+		}
 	} else {
 		fmt.Println("same-sex reproduction is prohibited")
 	}
