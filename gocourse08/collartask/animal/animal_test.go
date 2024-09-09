5daede996b0350da75dca6c731cba97dd5ea22c0
dmit <exehoua@gmail.com>
2024-09-09 19:27:12 +0300

    tests begin

--- gocourse08/collartask/animal/animal_test.go
+++ gocourse08/collartask/animal/animal_test.go
@@ -17,3 +17,12 @@ func TestNewAnimal(t *testing.T) {
 		t.Errorf("Expected AnimalType to be one of Bear, Tiger, or Lion, but got %v", animal.AnimalType)
 	}
 }
+
+func TestDataAnimal_RecordBreathing(t *testing.T) {
+}
+
+func TestDataAnimal_RecordAnimal(t *testing.T) {
+}
+
+func TestDataAnimal_Typify(t *testing.T) {
+}
