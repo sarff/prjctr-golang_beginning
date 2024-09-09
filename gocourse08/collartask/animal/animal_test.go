f5a325b2c92f7410634e4972f5a48b4865b06160
dmit <exehoua@gmail.com>
2024-09-09 23:25:26 +0300

    tests done

--- gocourse08/collartask/animal/animal_test.go
+++ gocourse08/collartask/animal/animal_test.go
@@ -1,6 +1,7 @@
 package animal
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -14,15 +15,56 @@ func TestNewAnimal(t *testing.T) {
 	animal := NewAnimal()
 
 	if !validAnimals[animal.AnimalType] {
-		t.Errorf("Expected AnimalType to be one of Bear, Tiger, or Lion, but got %v", animal.AnimalType)
+		t.Errorf("Expected AnimalType to be one of bear, tiger, or lion, but got %v", animal.AnimalType)
 	}
 }
 
 func TestDataAnimal_RecordBreathing(t *testing.T) {
+	animalData := DataAnimal[float64]{
+		AnimalType:  "lion",
+		Pulse:       100,
+		Temperature: 39,
+		Breathing:   []float64{},
+	}
+
+	breathData := 73.3
+
+	animalData.RecordBreathing(breathData)
+
+	wantData := []float64{breathData}
+	if !reflect.DeepEqual(animalData.Breathing, wantData) {
+		t.Errorf("DataAnimal.RecordBreathing() = %v, want %v", animalData.Breathing, wantData)
+	}
 }
 
 func TestDataAnimal_RecordAnimal(t *testing.T) {
+	animalData := DataAnimal[float64]{
+		AnimalType:  "lion",
+		Pulse:       100,
+		Temperature: 39,
+		Sounding:    []float64{},
+	}
+
+	soundData := 53.3
+
+	animalData.RecordSound(soundData)
+
+	wantData := []float64{soundData}
+	if !reflect.DeepEqual(animalData.Sounding, wantData) {
+		t.Errorf("DataAnimal.RecordSound() = %v, want %v", animalData.Sounding, wantData)
+	}
 }
 
 func TestDataAnimal_Typify(t *testing.T) {
+	validAnimals := map[string]bool{
+		"bear":  true,
+		"tiger": true,
+		"lion":  true,
+	}
+
+	animal := NewAnimal()
+	tapify := animal.Typify()
+	if !validAnimals[tapify] {
+		t.Errorf("Expected AnimalType to be one of bear, tiger, or lion, but got %v", tapify)
+	}
 }
